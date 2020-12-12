6b6a61aa95d4a3652938b2a6d8ceb687dd1c1d4e
Dima Grunitsky <dima.grunitsky@gmail.com>
2020-12-12 17:41:26 +0200

    Практична робота 9

---
+++ Fraction.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    public class Fraction
+    {
+        protected long Numer;
+        protected long Denom;
+
+        public double ToDouble()
+        {
+            var s = Numer + "," + Denom;
+
+            return Convert.ToDouble(s);
+        }
+
+        public override string ToString()
+        {
+            return this.ToDouble().ToString("n2");
+        }
+
+        public static Fraction ToFraction(double value)
+        {
+            var arr = value.ToString().Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+            return new Fraction()
+            {
+                Numer = Convert.ToInt32(arr[0]),
+                Denom = arr.Length == 2 ? Convert.ToUInt32(arr[1]) : 0
+            };
+        }
+
+        public static Fraction operator +(Fraction a, Fraction b)
+        {
+            return ToFraction(a.ToDouble() + b.ToDouble());
+        }
+        public static Fraction operator -(Fraction a, Fraction b)
+        {
+            return ToFraction(a.ToDouble() - b.ToDouble());
+        }
+        public static Fraction operator *(Fraction a, Fraction b)
+        {
+            return ToFraction(a.ToDouble() * b.ToDouble());
+        }
+        public static bool operator ==(Fraction a, Fraction b)
+        {
+            return a.Denom == b.Denom && a.Numer == b.Numer;
+        }
+        public static bool operator !=(Fraction a, Fraction b)
+        {
+            return a.Denom != b.Denom || a.Numer != b.Numer;
+        }
+        
+
+        }
+    }
+
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double value = 15.10;
+            var fr1 = Fraction.ToFraction(10.10);
+            var fr2 = Fraction.ToFraction(2.4);
+            Console.WriteLine((fr1 + fr2).ToDouble());
+            Console.WriteLine((fr1 - fr2).ToDouble());
+            Console.WriteLine((fr1 * fr2).ToDouble());
+            Console.WriteLine((fr1 == fr2));
+            Console.WriteLine((fr1 != fr2));
+        }
+    }
+}
